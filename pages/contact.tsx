0e77c623c199cdfa343d5068f342552b94316b75
Nick Forbes-Smith <nick@forbes-smith.com>
2023-10-01 17:42:45 +1100

    Add more form options

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -10,15 +10,32 @@ import ContactUsSubmittedMdx from "../content/contact-us-submitted.mdx";
 type Inputs = {
   nameRequired: string;
   emailRequired: string;
+  orgName?: string;
   phone?: number;
-  why: string;
+  socials?: string;
   detailsRequired: string;
 };
+
+enum FormReason {
+  Performance = "performance",
+  Workshops = "workshops",
+  VisualArt = "visual-art",
+  Stallholder = "stallholder",
+  Volunteer = "volunteer",
+  Other = "other",
+}
+
+const orgNameMap = new Map<FormReason, string>();
+orgNameMap.set(FormReason.Performance, "Act/DJ name");
+orgNameMap.set(FormReason.Workshops, "Workshop name");
+orgNameMap.set(FormReason.Stallholder, "Stall name");
 
 export default function Info() {
   const [formSubmitState, setFormSubmitState] = useState<
     "initial" | "loading" | "submitted" | "error"
   >("initial");
+
+  const [formReason, setFormReason] = useState<FormReason>();
 
   const {
     register,
@@ -32,6 +49,7 @@ export default function Info() {
     // turn data into FormData
     const formData = new FormData();
     Object.keys(data).forEach((key) => formData.append(key, data[key]));
+    if (formReason) formData.append("why", formReason.toString());
 
     fetch("https://usebasin.com/f/88247cd2f6e6", {
       method: "POST",
@@ -63,142 +81,208 @@ export default function Info() {
               <ContactUsIntroMdx />
               <div className="py-12">
                 <div className="mt-8 max-w-md">
-                  <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="grid grid-cols-1 gap-6">
-                      <label className="block">
-                        <span className="text-gray-100 font-mono">Name*</span>
-                        <input
-                          {...register("nameRequired", { required: true })}
-                          className={`font-mono font-light
-                    mt-0
-                    block
-                    w-full
-                    px-0.5 bg-transparent
-                    border-0 border-b-2 ${
-                      errors.nameRequired ? "border-red-400" : "border-gray-700"
-                    }
-                    focus:ring-0 focus:border-white
-                  `}
-                        />
-                        {errors.nameRequired && (
-                          <div className=" text-red-400 mt-2">
-                            This field is required
-                          </div>
-                        )}
-                      </label>
-                      <label className="block">
-                        <span className="text-gray-100 font-mono">
-                          Email address*
-                        </span>
-                        <input
-                          {...register("emailRequired", { required: true })}
-                          type="email"
-                          className={`
-                          font-mono font-light
-                    mt-0
-                    block
-                    w-full
-                    px-0.5 bg-transparent
-                    border-0 border-b-2 ${
-                      errors.emailRequired
-                        ? "border-red-400"
-                        : "border-gray-700"
-                    }
-                    focus:ring-0 focus:border-white
-                  `}
-                          placeholder="john@example.com"
-                        />
-                        {errors.emailRequired && (
-                          <div className=" text-red-400 mt-2">
-                            This field is required
-                          </div>
-                        )}
-                      </label>
-                      <label className="block">
-                        <span className="text-gray-100 font-mono">
-                          Phone number
-                        </span>
-                        <input
-                          {...register("phone")}
-                          type="tel"
-                          className={`font-mono font-light
-                    mt-0
-                    block
-                    w-full
-                    px-0.5 bg-transparent
-                    border-0 border-b-2 ${
-                      errors.phone ? "border-red-400" : "border-gray-700"
-                    }
-                    focus:ring-0 focus:border-white
-                  `}
-                        />
-                        {errors.phone && (
-                          <div className=" text-red-400 mt-2">
-                            This field is required
-                          </div>
-                        )}
-                      </label>
-                      <label className="block">
-                        <span className="text-gray-100 font-mono">
-                          Why are you contacting us?
-                        </span>
-                        <select
-                          defaultValue={"Performance/DJ"}
-                          {...register("why", { required: true })}
-                          className="font-mono font-light
-                    block
-                    w-full
-                    mt-0
-                    px-0.5 bg-transparent
-                    border-0 border-b-2 border-gray-700
-                    focus:ring-0 focus:border-white
-                  "
-                        >
-                          <option>Performance/DJ</option>
-                          <option>Workshops</option>
-                          <option>Visual Art</option>
-                          <option>Stallholder</option>
-                          <option>Volunteer</option>
-                          <option>Other</option>
-                        </select>
-                      </label>
-                      <label className="block mt-4">
-                        <span className="text-gray-100 font-mono">
-                          Details*
-                        </span>
-                        <textarea
-                          {...register("detailsRequired", { required: true })}
-                          className={`font-mono font-light
-                    mt-0
-                    block
-                    w-full
-                    px-0.5 bg-transparent
-                    border-0 border-b-2 
-                    ${
-                      errors.detailsRequired
-                        ? "border-red-400"
-                        : "border-gray-700"
-                    }
-                    focus:ring-0 focus:border-white
-                  `}
-                          rows={4}
-                        ></textarea>
-                        {errors.detailsRequired && (
-                          <div className=" text-red-400 mt-2">
-                            This field is required
-                          </div>
-                        )}
-                      </label>
-                      <button
-                        type="submit"
-                        className="h-10 px-5 text-indigo-100 bg-primary-600  font-mono  transition-colors duration-150 focus:shadow-outline hover:bg-primary-700"
-                      >
-                        Contact Us
-                      </button>
-                    </div>
-                  </form>
+                  <label className="block">
+                    <span className="text-gray-100 font-mono">
+                      Why are you contacting us?*
+                    </span>
+                    <select
+                      onChange={(e) => setFormReason(e.target.value as any)}
+                      value={formReason}
+                      className="font-mono font-light
+                        block
+                        w-full
+                        mt-0
+                        px-0.5 bg-transparent
+                        border-0 border-b-2 border-gray-700
+                        focus:ring-0 focus:border-white
+                      "
+                    >
+                      <option value="" disabled selected>
+                        Select a reason
+                      </option>
+                      <option value={FormReason.Performance}>
+                        Performance/DJ
+                      </option>
+                      <option value={FormReason.Workshops}>Workshops</option>
+                      <option value={FormReason.VisualArt}>Visual Art</option>
+                      <option value={FormReason.Stallholder}>
+                        Stallholder
+                      </option>
+                      <option value={FormReason.Volunteer}>Volunteer</option>
+                      <option value={FormReason.Other}>Other</option>
+                    </select>
+                  </label>
                 </div>
               </div>
+              {formReason ? (
+                <div className="py-12">
+                  <div className="mt-8 max-w-md">
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                      <div className="grid grid-cols-1 gap-6">
+                        <label className="block">
+                          <span className="text-gray-100 font-mono">
+                            Full name*
+                          </span>
+                          <input
+                            {...register("nameRequired", { required: true })}
+                            className={`font-mono font-light
+                          mt-0
+                          block
+                          w-full
+                          px-0.5 bg-transparent
+                          border-0 border-b-2 ${
+                            errors.nameRequired
+                              ? "border-red-400"
+                              : "border-gray-700"
+                          }
+                          focus:ring-0 focus:border-white
+                        `}
+                          />
+                          {errors.nameRequired && (
+                            <div className=" text-red-400 mt-2">
+                              This field is required
+                            </div>
+                          )}
+                        </label>
+                        {formReason === FormReason.Performance ||
+                        formReason === FormReason.Workshops ||
+                        formReason === FormReason.Stallholder ? (
+                          <label className="block">
+                            <span className="text-gray-100 font-mono">
+                              {orgNameMap.get(formReason) ??
+                                "Organisation Name"}
+                            </span>
+                            <input
+                              {...register("orgName")}
+                              className={`font-mono font-light
+                              mt-0
+                              block
+                              w-full
+                              px-0.5 bg-transparent
+                              border-0 border-b-2 ${
+                                errors.nameRequired
+                                  ? "border-red-400"
+                                  : "border-gray-700"
+                              }
+                              focus:ring-0 focus:border-white
+                            `}
+                            />
+                          </label>
+                        ) : null}
+                        <label className="block">
+                          <span className="text-gray-100 font-mono">
+                            Email address*
+                          </span>
+                          <input
+                            {...register("emailRequired", { required: true })}
+                            type="email"
+                            className={`
+                          font-mono font-light
+                          mt-0
+                          block
+                          w-full
+                          px-0.5 bg-transparent
+                          border-0 border-b-2 ${
+                            errors.emailRequired
+                              ? "border-red-400"
+                              : "border-gray-700"
+                          }
+                          focus:ring-0 focus:border-white
+                        `}
+                            placeholder="john@example.com"
+                          />
+                          {errors.emailRequired && (
+                            <div className=" text-red-400 mt-2">
+                              This field is required
+                            </div>
+                          )}
+                        </label>
+                        <label className="block">
+                          <span className="text-gray-100 font-mono">
+                            Phone number
+                          </span>
+                          <input
+                            {...register("phone")}
+                            type="tel"
+                            className={`font-mono font-light
+                          mt-0
+                          block
+                          w-full
+                          px-0.5 bg-transparent
+                          border-0 border-b-2 ${
+                            errors.phone ? "border-red-400" : "border-gray-700"
+                          }
+                          focus:ring-0 focus:border-white
+                        `}
+                          />
+                          {errors.phone && (
+                            <div className=" text-red-400 mt-2">
+                              This field is required
+                            </div>
+                          )}
+                        </label>
+                        <label className="block mt-4">
+                          <span className="text-gray-100 font-mono">
+                            Details*
+                          </span>
+                          <textarea
+                            placeholder="Please include anything we would need to provide you - e.g. DJ Equipment, Power, Water etc"
+                            {...register("detailsRequired", { required: true })}
+                            className={`font-mono font-light
+                              mt-0
+                              block
+                              w-full
+                              px-0.5 bg-transparent
+                              border-0 border-b-2 
+                              ${
+                                errors.detailsRequired
+                                  ? "border-red-400"
+                                  : "border-gray-700"
+                              }
+                              focus:ring-0 focus:border-white
+                            `}
+                            rows={4}
+                          ></textarea>
+                          {errors.detailsRequired && (
+                            <div className=" text-red-400 mt-2">
+                              This field is required
+                            </div>
+                          )}
+                        </label>
+                        {formReason === FormReason.Performance ||
+                        formReason === FormReason.VisualArt ||
+                        formReason === FormReason.Workshops ||
+                        formReason === FormReason.Stallholder ? (
+                          <label className="block mt-4">
+                            <span className="text-gray-100 font-mono">
+                              Social media links
+                            </span>
+                            <textarea
+                              {...register("socials")}
+                              className={`font-mono font-light
+                          mt-0
+                          block
+                          w-full
+                          px-0.5 bg-transparent
+                          border-0 border-b-2                          
+                          focus:ring-0 focus:border-white
+                        `}
+                              rows={4}
+                              placeholder="Facebook, Instagram, Soundcloud, Mixcloud, etc."
+                            ></textarea>
+                          </label>
+                        ) : null}
+                        <button
+                          type="submit"
+                          className="h-10 px-5 text-indigo-100 bg-primary-600  font-mono  transition-colors duration-150 focus:shadow-outline hover:bg-primary-700"
+                        >
+                          Contact Us
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              ) : null}
             </>
           ) : null}
 
