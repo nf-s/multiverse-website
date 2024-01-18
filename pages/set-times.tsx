2fb264f380bb0d59d1561ddc5285f876c9b3049d
Nick Forbes-Smith <nick@forbes-smith.com>
2024-01-19 10:36:54 +1100

    Fix set time width

--- pages/set-times.tsx
+++ pages/set-times.tsx
@@ -16,13 +16,13 @@ export default function Lineup() {
           <div>
             <h2>Main Stage</h2>
             <iframe
-              width="404px"
+              width="317px"
               height="3446px"
               style={{
                 margin: "32px 32px 0 0",
                 overflow: "hidden",
                 background: "none !important",
-                maxWidth: "404px",
+                maxWidth: "317px",
                 display: "inline",
               }}
               src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRW18YlU8ZiYNnZ2TvyxOCVJO5cX1o-FhvcHkrW6NT70Y3zSNfVT6bsN-Q-wpV_XQq-PamhR7_K_bry/pubhtml?gid=1552241499&amp;single=true&amp;widget=true&amp;headers=false"
@@ -31,13 +31,13 @@ export default function Lineup() {
           <div>
             <h2>Chillout Stage</h2>
             <iframe
-              width="404px"
+              width="317px"
               height="3446px"
               style={{
                 margin: "32px 0 0 0",
                 overflow: "hidden",
                 background: "none !important",
-                maxWidth: "404px",
+                maxWidth: "317px",
                 display: "inline",
               }}
               src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRW18YlU8ZiYNnZ2TvyxOCVJO5cX1o-FhvcHkrW6NT70Y3zSNfVT6bsN-Q-wpV_XQq-PamhR7_K_bry/pubhtml?gid=2006719255&amp;single=true&amp;widget=true&amp;headers=false"
