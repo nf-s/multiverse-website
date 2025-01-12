a12a9ef6dc40f30f06c52e412ffd81cf5933100f
Nick Forbes-Smith <nick@forbes-smith.com>
2025-01-12 14:33:21 +1100

    Disable contact

--- next.config.ts
+++ next.config.ts
@@ -24,7 +24,7 @@ const nextConfig: NextConfig = {
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
   publicRuntimeConfig: {
     UMAMI_WEBSITE_ID: process.env.UMAMI_WEBSITE_ID,
-    contactEnabled: true,
+    contactEnabled: false,
   },
 };
 
